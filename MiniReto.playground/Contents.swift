ab8a51d7ff2ca77a017b91f8a9c6371bda12ec17
chuckxyu <carlosgarciaordonez@gmail.com>
2016-03-28 00:19:18 -0600

    Mini Reto semana 4

--- MiniReto.playground/Contents.swift
+++ MiniReto.playground/Contents.swift
@@ -2,17 +2,51 @@
 
 import UIKit
 
-var numeros = 0...100
-for i in numeros {
-    if i%5 == 0 {
-        print("# \(i) Bingo!!!")
+var str = "Hello, playground"
+
+enum Velocidades: Int {
+    case apagado = 0,
+    velocidadBaja = 20,
+    velocidadMedia = 50,
+    velocidadAlta = 120
+    
+    init () {
+        self = Velocidades.apagado
     }
-    if i%2 == 0 {
-        print("# \(i) Par!!!")
-    } else {
-        print("# \(i) impar!!!")
+}
+
+class Auto {
+    var velocidad: Velocidades
+    
+    init (velocidad: Velocidades){
+        self.velocidad = velocidad
     }
-    if i >= 30 && i <= 40 {
-        print("# \(i) Viva Swift")
+    
+    func cambioDeVelocidad() -> (velocidad:Int, mensaje: String){
+        var mensaje = ""
+        if velocidad == Velocidades.apagado {
+            velocidad = Velocidades.velocidadBaja
+            mensaje = "Velocidad Baja"
+        } else if velocidad == Velocidades.velocidadBaja {
+            velocidad = Velocidades.velocidadMedia
+            mensaje = "Velocidad Media"
+        } else if velocidad == Velocidades.velocidadMedia {
+            velocidad = Velocidades.velocidadAlta
+            mensaje = "Velocidad Alta"
+        } else if velocidad == Velocidades.velocidadAlta {
+            velocidad = Velocidades.velocidadMedia
+            mensaje = "Velocidad Media"
+        }
+        
+        let resultado = (velocidad.rawValue, mensaje)
+        return resultado
     }
+}
+
+
+var automovil = Auto(velocidad: Velocidades.apagado)
+
+for var i = 0 ; i <= 20 ; i++ {
+    var velocidadActual = automovil.cambioDeVelocidad ()
+    print("\(velocidadActual.velocidad), \(velocidadActual.mensaje)")
 }
